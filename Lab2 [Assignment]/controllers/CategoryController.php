44a9ad09635671b3741022e91c2ffbedf9a00796
Md. Sadman Rahman <sadmanrman@gmail.com>
2021-07-14 02:55:42 +0600

    Added assignment

---
+++ Lab2 [Assignment]/controllers/CategoryController.php
@@ -0,0 +1,58 @@
+<?php
+	include 'models/db_config.php';
+
+	$catName = "";
+	$err_catName="";
+	$err_db="";
+	$hasError = false;
+	if(isset($_POST["add_Cat"])){
+		if(empty($_POST["catName"])){
+            $hasError = true;
+            $err_name = "*Category name Required";
+        }
+        
+        else{
+            $catName = $_POST["catName"];
+        }
+	
+	if(!$hasError){
+			$rs = insertCategory($catName);
+			if($rs === true){       
+				header("Location: HOMEPAGE.php");
+			}
+			$err_db = $rs;
+		}
+	}
+	else if (isset($_POST["edit_category"])){
+		
+            $rs = updateCategory($_POST["catName"],$_POST["id"]);
+			if($rs===true){
+				header("Location : allcategories.php");
+		}
+		$err_db = $rs;
+	
+	}
+	
+	function insertCategory($catName){
+			$query = "insert into category values (NULL, '$catName')";
+		return execute($query);
+		}
+	function getAllCategories(){
+		$query = "select * from category";
+		$rs = get($query);
+		return $rs;
+	}
+	function getCategory($id){
+		$query = "select * from category where id = $id";
+		$rs = get($query);
+		return $rs[0];
+	}
+	
+	function undateCategory($catName, $id)
+	{
+		$query= "update category set catName='$catName' where id = $id";
+		return execute($query);
+	}
+	
+	
+?>
---
+++ Lab2 [Assignment]/main_menu.php
@@ -0,0 +1,3 @@
+<html>
+	<body>
+	<center><a href = "editcategory.php">Add Category</a>&nbsp<a href = "allcategories.php">All Categories</a></center>
